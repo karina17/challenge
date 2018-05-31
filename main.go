a389a9dafbdd87806ff0ad5ffacba3fb5ca24e52
karina17 <karina.zholdasbekova@gmail.com>
2018-05-31 22:40:33 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/google/go-github/github"
@@ -12,7 +13,6 @@ import (
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
 	var versionSlice []*semver.Version
 	semver.Sort(releases)
-
 	// This is just an example structure of the code, if you implement this interface, the test cases in main_test.go are very easy to run
 	for _, r := range releases {
 		if !r.LessThan(*minVersion) {
@@ -21,10 +21,9 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 	}
 
 	var results []*semver.Version
+	var currentMajor = versionSlice[0].Major
+	var currentMinor = versionSlice[0].Minor
 	var currentVersion = versionSlice[0]
-	var currentMajor = currentVersion.Major
-	var currentMinor = currentVersion.Minor
-
 	for _, v := range versionSlice {
 		if v.Major != currentMajor || v.Minor != currentMinor {
 			currentMajor = v.Major
@@ -35,30 +34,32 @@ func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*s
 	}
 	results = append(results, currentVersion)
 
-	reverse(results)
-
-	return results
+	return reverse(results)
 }
 
-func reverse(a []*semver.Version) {
-	for i := len(a)/2-1; i >= 0; i-- {
-		opp := len(a)-1-i
-		a[i], a[opp] = a[opp], a[i]
+func reverse(numbers []*semver.Version) []*semver.Version {
+	if len(numbers) < 2 {
+		return numbers
 	}
+	newNumbers := make([]*semver.Version, len(numbers))
+	for i, j := 0, len(numbers)-1; i < j; i, j = i+1, j-1 {
+		newNumbers[i], newNumbers[j] = numbers[j], numbers[i]
+	}
+	return newNumbers
 }
 
 // Here we implement the basics of communicating with github through the library as well as printing the version
 // You will need to implement LatestVersions function as well as make this application support the file format outlined in the README
 // Please use the format defined by the fmt.Printf line at the bottom, as we will define a passing coding challenge as one that outputs
 // the correct information, including this line
-func getReleases(username string, repository string, minVersion *semver.Version) []*semver.Version {
+func getReleases(repository string, minVersion *semver.Version) []*semver.Version {
 	client := github.NewClient(nil)
 	ctx := context.Background()
 	opt := &github.ListOptions{PerPage: 10}
-	releases, _, err := client.Repositories.ListReleases(ctx, username, repository, opt)
+	repo := strings.Split(repository, "/")
+	releases, _, err := client.Repositories.ListReleases(ctx, repo[0], repo[1], opt)
 	if err != nil {
-		// return nil // is this really a good way?
-		fmt.Println(err.Error())
+		return nil // is this really a good way?
 	}
 
 	allReleases := make([]*semver.Version, len(releases))
@@ -74,14 +75,12 @@ func getReleases(username string, repository string, minVersion *semver.Version)
 func main() {
 	// Github
 	minVersion := semver.New("1.8.0")
-	allReleases := getReleases("kubernetes", "kubernetes", minVersion)
+	allReleases := getReleases("kubernetes/kubernetes", minVersion)
 	versionSliceK := LatestVersions(allReleases, minVersion)
-
 	minVersion = semver.New("2.2.0")
-	allReleases = getReleases("prometheus", "prometheus", minVersion)
+	allReleases = getReleases("prometheus/prometheus", minVersion)
 	versionSliceP := LatestVersions(allReleases, minVersion)
 
 	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSliceK)
 	fmt.Printf("\nlatest versions of prometheus/prometheus: %s", versionSliceP)
-
 }
